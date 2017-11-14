cccd6aca9835fe6fcd76abfa2c74ed121a96ce78
Abhiram Bitla <abhiram.bitla@gmail.com>
2017-11-14 21:30:30 +0530

    Intregated skills cia model genarator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113130602) do
+ActiveRecord::Schema.define(version: 20171114153401) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilizied"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
