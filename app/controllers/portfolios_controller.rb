8ee10157c18b0bab516171616d72a4d516f70e7d
Abhiram Bitla <abhiram.bitla@gmail.com>
2017-12-08 23:14:33 +0530

    Revert "Resolving conflict errors"
    This reverts commit 2e2955d8cf28a368f4c87d80c4e22b4996d557c9, reversing
    changes made to 8648e3ed2f9e01e6053576c409b625309bcbe4ca.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,4 @@
 class PortfoliosController < ApplicationController
-<<<<<<< HEAD
  def index
  	@portfolio_items = Portfolio.all
  end
@@ -54,27 +53,3 @@ end
 
 
 
-=======
-  def index
-    @portfolio_items = Portfolio.all
-  end
-
-  def new
-    @portfolio_items = Blog.new
-  end
-
-   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
-    respond_to do |format|
-      if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-end
-
-
->>>>>>> master
